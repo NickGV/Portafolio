2e57a278cb8749932d5f1aa751510f08aabb5568
Nick <nickgomvelez@gmail.com>
2024-08-29 11:51:21 -0500

    add icons files

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,18 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'background': '#0f0e17',
+        'paragraph': '#a7a9be',
+        'title': '#fffffe',
+        'button': '#ff8906',
+        'button-hover': '#ff8906',
+        'button-text': '#fffffe',
+        'button-text-active': '#fffffe',
+        'link-text': '#e53170',
+      }
+    },
   },
   plugins: [],
 }
